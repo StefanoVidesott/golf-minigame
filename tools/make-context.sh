272524e8e1e6137d20977b0bc97df5be3e81b376
Stefano Videsott <ste.videsott@gmail.com>
2025-08-13 17:28:28 +0200

    feat: moved build script and created script to write context to file

---
+++ tools/make-context.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if the "context" file already exists
+if [ -f "context.md" ]; then
+    echo "context.md already exists. Removing it..."
+    rm context.md
+else
+    echo "context.md does not exist. Creating a new one..."
+fi
+
+# Create the "context.md" file
+touch context.md
+echo "Creating context.md file..."
+# Find all files in the "include" directory and its subdirectories
+find src -type f | while read -r file; do
+    # Get the file name without the path
+    filename=$(basename "$file")
+    #only process files with specific extensions
+    if [[ ! "$filename" =~ \.(cpp|hpp)$ ]]; then
+        continue
+    fi
+    # Get the content of the file
+    content=$(cat "$file")
+    # Write the filename and content to the "context.md" file
+    echo "# File: $filename" >> context.md
+    echo "$content" >> context.md
+    echo "" >> context.md
+done
+
+tree -I "build" >> context.md
+
+echo "context.md file created successfully."
