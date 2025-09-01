62c4fbe0fa3b8b5052395900dfa9307532f1c63e
Stefano Videsott <ste.videsott@gmail.com>
2025-09-01 10:50:11 +0200

    feat: added unused resources

--- tools/create-context.sh
+++ tools/create-context.sh
@@ -13,7 +13,7 @@ touch context.md
 echo "Creating context.md file..."
 # Find all files in the "include" directory and its subdirectories
 echo "Engine and game using SFML 3.0.1" >> context.md
-find src -type f | while read -r file; do
+find "./src" -type f | grep -v "include/" | while read -r file; do
     # Get the file name without the path
     filename=$(basename "$file")
     #only process files with specific extensions
@@ -28,6 +28,6 @@ find src -type f | while read -r file; do
     echo "" >> context.md
 done
 
-tree -I "build" >> context.md
+tree src -L 3 -I "build/" -I "res/" -I "tools/" >> context.md
 
 echo "context.md file created successfully."
